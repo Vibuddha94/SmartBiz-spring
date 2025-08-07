c876c9ddd0cc54d415e8b7f7f486101056dfd988
Vibuddha94 <bvibuddha@gmail.com>
2025-08-07 17:33:27 +0530

    create and configure business service close #10

--- user/src/main/java/lk/acpt/user/UserApplication.java
+++ user/src/main/java/lk/acpt/user/UserApplication.java
@@ -2,9 +2,10 @@ package lk.acpt.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = {
-		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+		SecurityAutoConfiguration.class
 })
 public class UserApplication {
 
